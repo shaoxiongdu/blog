faa2cedf7e6ad65aa705ce24d09764a5bafe7c5b
ShaoxiongDu <shaoxiong.dev@aliyun.com>
2021-04-29 16:29:26 +0800

    update readme

---
+++ help/Github提交443.md
@@ -0,0 +1,3 @@
+git config --global --unset http.proxy
+
+git config --global --unset https.proxy
--- src/main/java/com/lrm/dao/BlogRepository.java
+++ src/main/java/com/lrm/dao/BlogRepository.java
@@ -22,7 +22,6 @@ public interface BlogRepository extends JpaRepository<Blog, Long>, JpaSpecificat
     @Query("select b from Blog b where b.title like ?1 or b.content like ?1")
     Page<Blog> findByQuery(String query,Pageable pageable);
 
-
     @Transactional
     @Modifying
     @Query("update Blog b set b.views = b.views+1 where b.id = ?1")
