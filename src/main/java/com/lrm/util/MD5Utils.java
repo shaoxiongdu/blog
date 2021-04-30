553ee54ffcf8377329fa8fabf60eded2fe3ec12b
ShaoxiongDu <shaoxiong.dev@aliyun.com>
2021-04-30 18:01:03 +0800

    update readme

--- src/main/java/com/lrm/util/MD5Utils.java
+++ src/main/java/com/lrm/util/MD5Utils.java
@@ -22,10 +22,12 @@ public class MD5Utils {
             StringBuffer buf = new StringBuffer("");
             for (int offset = 0; offset < byteDigest.length; offset++) {
                 i = byteDigest[offset];
-                if (i < 0)
+                if (i < 0) {
                     i += 256;
-                if (i < 16)
+                }
+                if (i < 16) {
                     buf.append("0");
+                }
                 buf.append(Integer.toHexString(i));
             }
             //32位加密
@@ -40,6 +42,6 @@ public class MD5Utils {
     }
 
     public static void main(String[] args) {
-        System.out.println(MD5Utils.code("admin"));
+        System.out.println(MD5Utils.code("dsx05117218"));
     }
 }
